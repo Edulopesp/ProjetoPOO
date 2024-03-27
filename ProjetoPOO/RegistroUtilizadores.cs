c82057a26aeea5e4f60c94479b7cb122f0cfeff3
User <User@DESKTOP-3IJFG6R>
2024-03-27 01:13:11 +0000

    Alteracoes minimas para melhoria de funcionalidadesAdicionar arquivos de projeto.

---
+++ ProjetoPOO/RegistroUtilizadores.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    class Utilizadores
+    {
+        public string NomeUtilizador;
+        public string EnderecoUtilizador;
+        public string TelefoneUtilizador;
+        public string PalavraChave;
+
+        public Utilizadores(string nome, string endereco, string telefone, string palavraChave)
+        {
+            NomeUtilizador = nome;
+            EnderecoUtilizador = endereco;
+            TelefoneUtilizador = telefone;
+            PalavraChave = palavraChave;
+        }
+
+        class Utilizador
+        {
+            public string NomeUtilizador { get; set; }
+
+            public Utilizador(string nome)
+            {
+                NomeUtilizador = nome;
+            }
+        }
+
+
+
+
+    }
+}
