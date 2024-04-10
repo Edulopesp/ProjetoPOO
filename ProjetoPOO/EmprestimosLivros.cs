5c81e2dc354c2ac22eb8a7b1cbc168044adca09e
synystry1989 <156231647+synystry1989@users.noreply.github.com>
2024-04-10 09:27:19 +0100

    ColocadosMetodosNaClasse
    emprestimos livros com os metodos todos na classe

--- ProjetoPOO/EmprestimosLivros.cs
+++ ProjetoPOO/EmprestimosLivros.cs
@@ -13,9 +13,6 @@ namespace ProjetoPOO
         public string NomeCliente;
         public RegistarLivro Livro;
         public int Duracao;
-        public int QuantidadeExemplaresExistente;
-        //public int QuantidadeLivroAlugado;
-        //public int NrVezesAlugado;
         public bool Devolvido = false;
         public DateOnly Data; //{ get; set; }
 
@@ -26,25 +23,98 @@ namespace ProjetoPOO
             NomeCliente = nomeCliente;
             Livro = livro;
             Duracao = duracao;
-            //QuantidadeExemplaresExistente = quantidadeExemplaresExistente;
-            //QuantidadeLivroAlugado = quantidadeLivroAlugado;
+           
             Devolvido = devolvido;
             Data = data;
-
-            // NrVezesAlugado = nrVezesAlugado;
         }
         public EmprestimosLivros() 
         {
         }
-        public void LerPedidoAluguer()
+        public static void LerPedidoAluguer(List<RegistarLivro>Livros,Utilizadores utilizadorLogado, List<EmprestimosLivros> emprestimoLivros)
         {
+            do
+            {
+                Console.WriteLine("Qual o livro que deseja alugar? ");
+                string tituloLivroAluguer = Console.ReadLine();
+
+                RegistarLivro livroEscolhido = Livros.Find(a => a.NomeLivro == tituloLivroAluguer);
+
+                if (livroEscolhido != null)
+                {
+                    Console.WriteLine("Quantos dias de aluguer deseja? ");
+                    int duracaoLivroAluguer = int.Parse(Console.ReadLine());
+
+                    DateOnly dataLivroAluguer = DateOnly.FromDateTime(DateTime.Now);
+
+                    //Console.WriteLine("Quantos exemplares pretende alugar?"); 
+                    //int qntLivroAluguer = int.Parse(Console.ReadLine());
+
+                    string nomeClienteAluguer = utilizadorLogado.NomeUtilizador;
+
+                    emprestimoLivros.Add(new EmprestimosLivros(utilizadorLogado.NomeUtilizador, livroEscolhido, duracaoLivroAluguer, false, dataLivroAluguer));
+
+                    livroEscolhido.NumExemp--;
+
+                    //livroEscolhido.NumVezesEscolhido++
+
+                    //retornamos o livro escolhido como registar livro para ? sair da funcao ?
+
+                    //return livroEscolhido;
+                   break; //inverter os ifs talvez o break salte fora do while 
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Livro não encontrado, tente novamente.");
+                    Console.WriteLine("");
+                }
+            } while (true);
+        }
+
+        public static void DevolucaoLivroAluguer(Utilizadores utilizadorLogado,List<EmprestimosLivros>emprestimoLivros,List<RegistarLivro>Livros, RegistarLivro livro)
+        {
+            Console.WriteLine("--------------------------------------------- Os seus Livros ---------------------------------------------");
+            foreach (EmprestimosLivros book in emprestimoLivros)
+            {
+                if ((book.NomeCliente == utilizadorLogado.NomeUtilizador) && (book.Devolvido == false))
+                {
+                    book.ConsultaEmprestimoIndividual();
+                }
+                else
+                {
+                    Console.WriteLine("Não há livros alugados na sua conta.");
+                    MenuPrincipal.MenuAcoesPrincipal(utilizadorLogado,Livros,emprestimoLivros,livro);
+                }
+
+            }
+            Console.WriteLine("==========================================================================================================");
+
+            Console.WriteLine("Qual o livro que deseja devolver? ");
+            string livroAremover = Console.ReadLine();
+
+            //lemos o nome do objeto novo atraves de uma variavel e vamos a procura de onde na lista um objeto tem esse nome
+            EmprestimosLivros DevolverLivro = emprestimoLivros.Find(x => x.Livro.NomeLivro == livroAremover);
+            Console.WriteLine();
+            if (livroAremover != null)
+            {//atribuimos o estado verdadeiro ao atributo da nossa classe emprestimosLivros
+                //adicionamos novamente a quantidade ao registo do livro devolvido
+                DevolverLivro.Devolvido = true;
+                DevolverLivro.Livro.NumExemp++;
+                Console.WriteLine("Livro devolvido com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Livro nao encontrado");
+            }
         }
         public void ConsultaListaEmpr()
         {
             //objetivo do nr vezes alugado seria para fazer um best of livros mais requisitados
+            
+
             Console.WriteLine("---------------------------------------------------------------------------------------------");
             Console.WriteLine($"| Titulo: {Livro.NomeLivro, -10} | Nome Cliente: {NomeCliente,-10} | Duracao: {Duracao} | Data Pedido: {Data,-10} |");
-            //  + "Nr vezes Alugado: " + QuantidadeLivroAlugado
+           
 
         }
         public void ConsultaEmprestimoIndividual()
@@ -52,15 +122,15 @@ namespace ProjetoPOO
             Console.WriteLine("------------------------------------------------------------------------");
             Console.WriteLine($"| Título: {Livro.NomeLivro,-9} | Duração: {Duracao,-9} | Data Pedido: {Data,-9} |");
         }
-
-        /*
-       public void AtualizarQuantidadeLista(RegistarLivro livro)
-         {
-
-              QuantidadeLivroAlugado -= livro.NumExemp;
-         }
-
-         */
-
+        // no relatorio poderia vir a informacao  se o livro foi devolvido a tempo etc
+        public static void RelatorioEmprestimos(List<EmprestimosLivros>emprestimoLivros)
+        {
+            Console.WriteLine("-------------------------------------- Lista de Emprestimos ---------------------------------");
+            foreach (var livroAlugado in emprestimoLivros)
+            {
+                livroAlugado.ConsultaListaEmpr();
+            }
+            Console.WriteLine("============================================================================================="); // ajustar tamanho dps
+        }
     }
 }
