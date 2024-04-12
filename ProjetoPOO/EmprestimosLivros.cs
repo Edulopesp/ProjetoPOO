b9ebe97e638ceb8ef76c56212aee1a5854520cff
synystry1989 <156231647+synystry1989@users.noreply.github.com>
2024-04-12 12:43:41 +0100

    tabelas+filtro

--- ProjetoPOO/EmprestimosLivros.cs
+++ ProjetoPOO/EmprestimosLivros.cs
@@ -81,146 +81,150 @@ namespace ProjetoPOO
                         Console.WriteLine();
                         Console.WriteLine("Livro não encontrado, tente novamente.");
                     }
-            } while (true);
-              return livroVazio;
+                }
+                while (true);
+            }
+            return livroVazio;
         }
 
-        public static void DevolucaoLivroAluguer(Utilizadores utilizadorLogado,List<EmprestimosLivros>emprestimoLivros,List<RegistarLivro>Livros, List<Utilizadores> listaUtilizadores)
-        {   
-            int contadorLivrosEmprestados = 0;
-            Console.WriteLine("============================================= Os seus Livros =============================================");
 
-            foreach (EmprestimosLivros livro in emprestimoLivros)
-            {
-                if ((livro.NomeCliente == utilizadorLogado.NomeUtilizador) && (livro.Devolvido == false))
+                public static void DevolucaoLivroAluguer(Utilizadores utilizadorLogado, List<EmprestimosLivros> emprestimoLivros, List<RegistarLivro> Livros, List<Utilizadores> listaUtilizadores)
                 {
-                    livro.ConsultaEmprestimoIndividual();
-                    contadorLivrosEmprestados++;
+                    int contadorLivrosEmprestados = 0;
+                    Console.WriteLine("============================================= Os seus Livros =============================================");
+
+                    foreach (EmprestimosLivros livro in emprestimoLivros)
+                    {
+                        if ((livro.Cliente.NomeUtilizador == utilizadorLogado.NomeUtilizador) && (livro.Devolvido == false))
+                        {
+                            livro.ConsultaEmprestimoIndividual();
+                            contadorLivrosEmprestados++;
+                        }
+                    }
+                    if (contadorLivrosEmprestados == 0)
+                    {
+                        Console.WriteLine("Não há livros alugados na sua conta.");
+                        MenuPrincipal.MenuAcoesPrincipal(listaUtilizadores, utilizadorLogado, Livros, emprestimoLivros);
+                    }
+
+                    Console.WriteLine("|_________________________________________________________________________________________________________|");
+                    consultaAlugueresCliente(utilizadorLogado, emprestimoLivros, Livros, listaUtilizadores);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Qual o livro que deseja devolver? ");
+                    string livroAremover = Console.ReadLine();
+
+                    var dataHoje = DateOnly.FromDateTime(DateTime.Now);
+
+                    //lemos o nome do objeto novo atraves de uma variavel e vamos a procura de onde na lista um objeto tem esse nome
+                    EmprestimosLivros DevolverLivro = emprestimoLivros.Find(x => x.Livro.NomeLivro == livroAremover);
+                    Console.WriteLine();
+                    if (DevolverLivro != null)
+                    {//atribuimos o estado verdadeiro ao atributo da nossa classe emprestimosLivros
+                     //adicionamos novamente a quantidade ao registo do livro devolvido
+                        DevolverLivro.Devolvido = true;
+                        DevolverLivro.Livro.NumExemp++;
+                        Console.Clear();
+
+                        int diasCalculo = DevolverLivro.DataEntregaLivroAlugado().DayNumber - dataHoje.DayNumber;
+
+                        if (diasCalculo > 0)
+                        {
+                            Console.WriteLine("Livro devolvido com sucesso");
+                        }
+                        else
+                        {
+                            DevolverLivro.Cliente.Penalizado = dataHoje.DayNumber;
+                            Console.WriteLine("Livro entregue com atraso, penalizado por 3 dias sem possibilidade de aluguer.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Livro nao encontrado");
+                    }
+                }
+                public void ConsultaListaEmpr()
+                {
+                    //objetivo do nr vezes alugado seria para fazer um best of livros mais requisitados
+                    Console.WriteLine("|------------------------------------------------------------------------------------------------------------------|");
+                    Console.WriteLine($"| Titulo: {Livro.NomeLivro,-10} | Nome Cliente: {Cliente.NomeUtilizador,-10} | Duracao: {Duracao}  | Data Pedido: {Data,-10} | Status: {statusEmprestimo(),-12} |");
+                    Console.WriteLine("|------------------------------------------------------------------------------------------------------------------|");
+                }
+                public void ConsultaEmprestimoIndividual()
+                {
+                    var dataHoje = DateOnly.FromDateTime(DateTime.Now);
+
+                    Console.WriteLine("|----------------------------------------------------------------------------------|");
+                    Console.WriteLine($"| Título: {Livro.NomeLivro,-9} | Duração: {Duracao,-2} | Data Pedido: {Data,-9} | {calculoDiasParaEntrega()} |");
+                    Console.WriteLine("|----------------------------------------------------------------------------------|");
+                }
+                // no relatorio poderia vir a informacao  se o livro foi devolvido a tempo etc
+                public DateOnly DataEntregaLivroAlugado()
+                {
+                    var dataLimite = Data.AddDays(Duracao);
+                    return dataLimite;
+                }
+                public static void consultaAlugueresCliente(Utilizadores utilizadorLogado, List<EmprestimosLivros> emprestimoLivros, List<RegistarLivro> Livros, List<Utilizadores> listaUtilizadores)
+                {
+                    int contadorLivrosDoUtilizador = 0;
+
+                    Console.WriteLine("======================================== Os seus Livros =============================================");
+
+                    foreach (EmprestimosLivros livro in emprestimoLivros)
+                    {
+                        if ((livro.Cliente.NomeUtilizador == utilizadorLogado.NomeUtilizador) && (livro.Devolvido == false))
+                        {
+                            livro.ConsultaEmprestimoIndividual();
+                            contadorLivrosDoUtilizador++;
+                        }
+                    }
+                    if (contadorLivrosDoUtilizador == 0)
+                    {
+                        Console.WriteLine("Não há livros alugados na sua conta.");
+                        MenuPrincipal.MenuAcoesPrincipal(listaUtilizadores, utilizadorLogado, Livros, emprestimoLivros);
+                    }
+                    Console.WriteLine("|___________________________________________________________________________________________________|");
+                }
+                public string statusEmprestimo()
+                {
+                    var dataHoje = DateOnly.FromDateTime(DateTime.Now);
+                    string Status;
+
+                    if (Devolvido == true)
+                    {
+                        Status = "Finalizado";
+                        return Status;
+                    }
+                    else
+                    {
+                        if (DataEntregaLivroAlugado().DayNumber - dataHoje.DayNumber < 0)
+                        {
+                            Status = "Atrasado";
+                            return Status;
+                        }
+                        else
+                        {
+                            Status = "Em Andamento";
+                            return Status;
+                        }
+                    }
+                }
+                public string calculoDiasParaEntrega()
+                {
+                    var dataHoje = DateOnly.FromDateTime(DateTime.Now);
+                    string respostaCalculo;
+
+                    int diasCalculo = DataEntregaLivroAlugado().DayNumber - dataHoje.DayNumber;
+                    if (diasCalculo > 0)
+                    {
+                        respostaCalculo = $"Entregar em: {diasCalculo} dias";
+                        return respostaCalculo;
+                    }
+                    else
+                    {
+                        respostaCalculo = "Status: Atrasado";
+                        return respostaCalculo;
+                    }
                 }
             }
-            if (contadorLivrosEmprestados == 0)
-              {
-                Console.WriteLine("Não há livros alugados na sua conta.");
-                MenuPrincipal.MenuAcoesPrincipal(listaUtilizadores, utilizadorLogado, Livros, emprestimoLivros);
-              }
-
-            Console.WriteLine("_________________________________________________________________________________________________________|");
-            consultaAlugueresCliente(utilizadorLogado, emprestimoLivros, Livros, listaUtilizadores);
-
-            Console.WriteLine();
-            Console.WriteLine("Qual o livro que deseja devolver? ");
-            string livroAremover = Console.ReadLine();
-
-            var dataHoje = DateOnly.FromDateTime(DateTime.Now);
-
-            //lemos o nome do objeto novo atraves de uma variavel e vamos a procura de onde na lista um objeto tem esse nome
-            EmprestimosLivros DevolverLivro = emprestimoLivros.Find(x => x.Livro.NomeLivro == livroAremover);
-            Console.WriteLine();
-            if (DevolverLivro != null)
-            {//atribuimos o estado verdadeiro ao atributo da nossa classe emprestimosLivros
-                //adicionamos novamente a quantidade ao registo do livro devolvido
-                DevolverLivro.Devolvido = true;
-                DevolverLivro.Livro.NumExemp++;
-                Console.Clear();
-
-                int diasCalculo = DevolverLivro.DataEntregaLivroAlugado().DayNumber - dataHoje.DayNumber;
-
-                if (diasCalculo > 0)
-                {
-                    Console.WriteLine("Livro devolvido com sucesso");
-                }
-                else
-                {
-                    DevolverLivro.Cliente.Penalizado = dataHoje.DayNumber;
-                    Console.WriteLine("Livro entregue com atraso, penalizado por 3 dias sem possibilidade de aluguer.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Livro nao encontrado");
-            }
         }
-        public void ConsultaListaEmpr()
-        {
-            //objetivo do nr vezes alugado seria para fazer um best of livros mais requisitados
-            Console.WriteLine("|------------------------------------------------------------------------------------------------------------------|");
-            Console.WriteLine($"| Titulo: {Livro.NomeLivro,-10} | Nome Cliente: {Cliente.NomeUtilizador,-10} | Duracao: {Duracao}  | Data Pedido: {Data,-10} | Status: {statusEmprestimo(),-12} |");
-            Console.WriteLine("|------------------------------------------------------------------------------------------------------------------|");
-        }
-        public void ConsultaEmprestimoIndividual()
-        {
-            var dataHoje = DateOnly.FromDateTime(DateTime.Now);
-
-            Console.WriteLine("|----------------------------------------------------------------------------------|");
-            Console.WriteLine($"| Título: {Livro.NomeLivro,-9} | Duração: {Duracao,-2} | Data Pedido: {Data,-9} | {calculoDiasParaEntrega()} |");
-            Console.WriteLine("|----------------------------------------------------------------------------------|");
-        }
-        // no relatorio poderia vir a informacao  se o livro foi devolvido a tempo etc
-        public DateOnly DataEntregaLivroAlugado()
-        {
-            var dataLimite = Data.AddDays(Duracao);
-            return dataLimite;
-        }
-        public static void consultaAlugueresCliente(Utilizadores utilizadorLogado, List<EmprestimosLivros> emprestimoLivros, List<RegistarLivro> Livros, List<Utilizadores> listaUtilizadores)
-        {
-            int contadorLivrosDoUtilizador = 0;
-
-            Console.WriteLine("======================================== Os seus Livros =============================================");
-
-            foreach (EmprestimosLivros livro in emprestimoLivros)
-            {
-                if ((livro.Cliente.NomeUtilizador == utilizadorLogado.NomeUtilizador) && (livro.Devolvido == false))
-                {
-                    livro.ConsultaEmprestimoIndividual();
-                    contadorLivrosDoUtilizador++;
-                }
-            }
-            if (contadorLivrosDoUtilizador == 0)
-            {
-                Console.WriteLine("Não há livros alugados na sua conta.");
-                MenuPrincipal.MenuAcoesPrincipal(listaUtilizadores, utilizadorLogado, Livros, emprestimoLivros);
-            }
-            Console.WriteLine("|___________________________________________________________________________________________________|");
-        }
-        public string statusEmprestimo()
-        {
-            var dataHoje = DateOnly.FromDateTime(DateTime.Now);
-            string Status;
-
-            if (Devolvido == true)
-            {
-                Status = "Finalizado";
-                return Status;
-            }
-            else
-            {
-                if (DataEntregaLivroAlugado().DayNumber - dataHoje.DayNumber < 0)
-                {
-                    Status = "Atrasado";
-                    return Status;
-                }
-                else
-                {
-                    Status = "Em Andamento";
-                    return Status;
-                }
-            }
-        }
-        public string calculoDiasParaEntrega()
-        {
-            var dataHoje = DateOnly.FromDateTime(DateTime.Now);
-            string respostaCalculo;
-
-            int diasCalculo = DataEntregaLivroAlugado().DayNumber - dataHoje.DayNumber;
-            if (diasCalculo > 0) 
-            {
-                respostaCalculo = $"Entregar em: {diasCalculo} dias";
-                return respostaCalculo;
-            } else
-            {
-                respostaCalculo = "Status: Atrasado";
-                return respostaCalculo;
-            }
-        }
-    }
-}
